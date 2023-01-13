d64890495319069e6ee2ec8db5a6509d634c7191
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 11:22:51 -0300

    Feat: Added alerts to error and success flashs

--- plum_blog/templates/element/flash/error.php
+++ plum_blog/templates/element/flash/error.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * @var \App\View\AppView $this
- * @var array $params
- * @var string $message
- */
 if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message error" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Error!</strong> <?= $message ?>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
--- plum_blog/templates/element/flash/success.php
+++ plum_blog/templates/element/flash/success.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * @var \App\View\AppView $this
- * @var array $params
- * @var string $message
- */
 if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message success" onclick="this.classList.add('hidden')"><?= $message ?></div>
+
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> <?= $message ?>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
