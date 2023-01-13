09d4a2431d4856ab9c5d1c36b98942cd1b748b0a
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 11:28:39 -0300

    Feat: adds UsersController index view

---
+++ plum_blog/templates/Admin/Users/index.php
@@ -0,0 +1,10 @@
+<h1>
+    Users
+    <div class= "float-right">
+        <?= $this->Html->link(
+            'Add',
+            ['action' => 'add'],
+            ['class' => 'btn btn-success']
+            ) ?>
+    </div>
+</h1>
