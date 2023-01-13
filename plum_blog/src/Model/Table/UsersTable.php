906ee585f5ab41f3d4a012357198b768af34527a
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 11:30:18 -0300

    Feat: creates UsersTable

---
+++ plum_blog/src/Model/Table/UsersTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class UsersTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator->requirePresence('first_name', 'create', 'This field must be filled in')
+            ->requirePresence('last_name', 'create', 'This field must be filled in')
+            ->requirePresence('email', 'create', 'This field must be filled in')
+            ->requirePresence('password', 'create', 'This field must be filled in')
+
+            ->notEmptyString('fist_name', 'This field must be filled in')
+            ->notEmptyString('last_name', 'This field must be filled in')
+            ->notEmptyString('email', 'This field must be filled in')
+            ->notEmptyString('password', 'This field must be filled in')
+
+            ->email('email', true, 'Your e-mail is invalid')
+            ->minLength('password', 6, 'Passwords should be at least 6 characters long')
+
+            ->allowEmptyFile('image')
+            ->uploadedFile('image', [
+                'types' => ['image/jpeg', 'image/png'],
+                'maxSize' => 2000000
+            ]);
+
+
+        return $validator;
+    }
+}
