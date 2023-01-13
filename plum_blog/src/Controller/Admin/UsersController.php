bbb163a22dd70a2d3698e5bce0c5bfcd3a5863d9
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 14:47:47 -0300

    Feat: Adds remove() and edit(). (add and edit not working as intended)

--- plum_blog/src/Controller/Admin/UsersController.php
+++ plum_blog/src/Controller/Admin/UsersController.php
@@ -7,9 +7,12 @@ use App\Controller\AppController;
 
 class UsersController extends AppController
 {
-    public function index()
+    public function index(): void
     {
+        $users = $this->Users->find('all')
+        ->order(['first_name']);
 
+        $this->set('users', $users);
     }
 
     public function add()
@@ -20,14 +23,48 @@ class UsersController extends AppController
             $user = $this->Users->patchEntity($user, $this->request->getData());
 
             if ($this->Users->save($user)) {
-                $this->Flash->success('User successfully registered!');
+                //$this->Flash->success('User successfully registered!');
 
-                return $this->redirect(['controller' => 'Users','action' => 'index']);
+                return $this->redirect(['action' => 'index']);
             }
 
-            $this->Flash->error('There was an error, please check the form');
+            //$this->Flash->error('There was an error, please check the form');
         }
 
         $this->set('user', $user);
+    }
+
+    public function edit($id)
+    {
+        $user = $this->Users->get($id);
+
+        if ($this->request->is('put')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+
+            if ($this->Users->save($user)) {
+                // $this->Flash->success('User successfully registered!');
+
+                return $this->redirect($this->referer());
+            }
+
+            // $this->Flash->error('There was an error, please check the form');
+        }
+
+        $this->set('user', $user);
+    }
+
+    public function remove($id)
+    {
+        $this->request->allowMethod('post');
+
+        $user = $this->Users->get($id);
+
+        if ($this->Users->delete($user)) {
+            $this->Flash->success('User removed');
+        } else {
+            $this->Flash->error('It was not possible to remove the user');
+        }
+
+        return $this->redirect($this->referer());
     }
 }
