4e85135853992553fcfe2ae6200476d531af5b96
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 14:53:58 -0300

    Feat: creates edit template

---
+++ plum_blog/templates/Admin/Users/edit.php
@@ -0,0 +1,13 @@
+<h1>Edit user <?= $user->first_name . $user->last_name ?></h1>
+
+<?php
+echo $this->Form->create($user, ['type' => 'file']);
+echo $this->Form->control('first_name');
+echo $this->Form->control('last_name');
+echo $this->Form->control('email', ['label' => 'E-Mail']);
+echo $this->Form->control('password', ['type' => 'password']);
+echo $this->Form->control('image', ['type' => 'file']);
+echo $this->Flash->render();
+echo $this->Form->button('Add');
+echo $this->Form->end();
+?>
