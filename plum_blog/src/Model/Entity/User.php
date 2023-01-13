3c8ad7c3f121d21112eb55dab390c301c4a6069d
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 14:48:09 -0300

    Feat: creates User entity

---
+++ plum_blog/src/Model/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
+
+
+class User extends Entity
+{
+    public $_accessible = [
+        '*'
+    ];
+
+    public function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+
+    public function _getFullName()
+    {
+        return $this->_properties['first_name'] . $this->_properties['last_name'];
+    }
+}
