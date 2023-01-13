db5d1ddb1abb6dc9342e79fec15045ec8a7a5350
Edson Pimenta <edson.tibo@gmail.com>
2023-01-13 14:54:32 -0300

    Feat: creates user list

--- plum_blog/templates/Admin/Users/index.php
+++ plum_blog/templates/Admin/Users/index.php
@@ -8,3 +8,37 @@
             ) ?>
     </div>
 </h1>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th width="40">#</th>
+            <th>Name</th>
+            <th width="170">Modified</th>
+            <th width="40"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($users as $user) : ?>
+        <tr>
+            <td><?= $user->id ?></td>
+            <td>
+                <?= $this->Html->link(
+                $user->first_name . ' ' . $user->last_name,
+                ['action' => 'edit', $user->id]
+                ) ?>
+            </td>
+            <td>
+                <?= $user->modified->nice() ?>
+            </td>
+            <td>
+                <?= $this->Form->postLink(
+                    'Remove',
+                    ['action' => 'remove', $user->id],
+                    ['class' => 'btn btn-danger btn-sm', 'confirm' => 'Are you sure you want to remove this user?']
+                ) ?>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
