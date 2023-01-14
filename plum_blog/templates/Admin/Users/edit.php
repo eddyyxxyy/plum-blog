7aa83452d49d9fd4bde75666fc383ce56e4e5ccb
Edson Pimenta <edson.tibo@gmail.com>
2023-01-14 15:31:30 -0300

    Fix: removed echo from create(), render() and end()

--- plum_blog/templates/Admin/Users/edit.php
+++ plum_blog/templates/Admin/Users/edit.php
@@ -1,13 +1,13 @@
-<h1>Edit user <?= $user->first_name . $user->last_name ?></h1>
+<h1>Edit user <?= "$user->first_name $user->last_name" ?></h1>
 
 <?php
-echo $this->Form->create($user, ['type' => 'file']);
+$this->Form->create($user, ['type' => 'file']);
 echo $this->Form->control('first_name');
 echo $this->Form->control('last_name');
 echo $this->Form->control('email', ['label' => 'E-Mail']);
 echo $this->Form->control('password', ['type' => 'password']);
 echo $this->Form->control('image', ['type' => 'file']);
-echo $this->Flash->render();
+$this->Flash->render();
 echo $this->Form->button('Add');
-echo $this->Form->end();
+$this->Form->end();
 ?>
