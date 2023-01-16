b2c969114bb41b351c81dfe036c2148157588137
Edson Pimenta <edson.tibo@gmail.com>
2023-01-16 11:23:47 -0300

    Fix: fix button margin

--- plum_blog/templates/Admin/Users/add.php
+++ plum_blog/templates/Admin/Users/add.php
@@ -7,7 +7,11 @@ echo $this->Form->control('last_name');
 echo $this->Form->control('email', ['label' => 'E-Mail']);
 echo $this->Form->control('password', ['type' => 'password']);
 echo $this->Form->control('image', ['type' => 'file']);
-echo $this->Flash->render();
-echo $this->Form->button('Add');
-echo $this->Form->end();
 ?>
+<div class="mt-3">
+    <?php
+    echo $this->Flash->render();
+    echo $this->Form->button('Add');
+    ?>
+</div>
+<?php echo $this->Form->end();?>
