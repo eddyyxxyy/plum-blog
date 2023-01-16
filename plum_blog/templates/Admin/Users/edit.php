99fe59cffc9b20761147ddb0e092ba30a8341712
Edson Pimenta <edson.tibo@gmail.com>
2023-01-16 10:20:54 -0300

    Fix: adds echo to all Form calls and fixes Flash render

--- plum_blog/templates/Admin/Users/edit.php
+++ plum_blog/templates/Admin/Users/edit.php
@@ -1,13 +1,13 @@
 <h1>Edit user <?= "$user->first_name $user->last_name" ?></h1>
 
 <?php
-$this->Form->create($user, ['type' => 'file']);
+echo $this->Form->create($user, ['type' => 'file']);
 echo $this->Form->control('first_name');
 echo $this->Form->control('last_name');
 echo $this->Form->control('email', ['label' => 'E-Mail']);
 echo $this->Form->control('password', ['type' => 'password']);
 echo $this->Form->control('image', ['type' => 'file']);
-$this->Flash->render();
-echo $this->Form->button('Add');
-$this->Form->end();
+echo $this->Flash->render();
+echo $this->Form->button(__('Add'));
+echo $this->Form->end();
 ?>
