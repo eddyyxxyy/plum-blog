5d6e0a67535fb59815438d74a13e3d0646649e67
Edson Pimenta <edson.tibo@gmail.com>
2023-01-16 12:09:12 -0300

    Feat: creates Categories Table

---
+++ plum_blog/src/Model/Table/CategoriesTable.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+
+class CategoriesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('categories');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Slug');
+        $this->addBehavior('Timestamp');
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator->requirePresence('name', 'create', 'This field must be filled in')
+
+            ->notEmptyString('name', 'This field must be filled in');
+
+        return $validator;
+    }
+}
