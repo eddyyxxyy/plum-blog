f2144f0b6ecdd033062d1f2287ad8ad9e116556c
Edson Pimenta <edson.tibo@gmail.com>
2023-01-17 14:47:05 -0300

    Feat: creates pagination for Categories Controller

--- plum_blog/src/Controller/CategoriesController.php
+++ plum_blog/src/Controller/CategoriesController.php
@@ -7,8 +7,18 @@ use App\Controller\AppController;
 
 class CategoriesController extends AppController
 {
+    public $paginate = [
+        'order' => ['Posts.modified' => 'desc'],
+    ];
+
     public function view()
     {
-
+        $this->set([
+            'posts' => $this->paginate(
+                $this->Categories->Posts->find('all')
+                    ->contain(['Categories', 'Users'])
+                    ->where(['Categories.slug' => $this->request->getParam('category')])
+            )
+        ]);
     }
 }
