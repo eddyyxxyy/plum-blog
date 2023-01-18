ea300e7e21457943f8c9bba3182b7667b6677f24
Edson Pimenta <edson.tibo@gmail.com>
2023-01-18 08:53:00 -0300

    Feat: creates full-page template

---
+++ plum_blog/templates/layout/full-page.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $this->fetch('title') ?></title>
+    <meta name="description" content="<?= $this->fetch('description') ?>">
+
+    <?= $this->Html->css([
+        'bootstrap.min',
+    ]) ?>
+</head>
+<body>
+
+    <?= $this->Flash->render() ?>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                Plum Blog
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <?= $this->Html->link(
+                            'About us',
+                            ['controller' => 'about-us', 'action' => ''],
+                            ['class' => 'nav-link']
+                        ) ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <?= $this->cell('Categories')->render('menu') ?>
+
+    <div class="container p-5">
+        <?= $this->fetch('content') ?>
+    </div>
+
+    <div class="p-5 bg-dark text-secondary">
+        <p class="mb-0">Copyright &copy; <?= date('Y') ?></p>
+    </div>
+
+    <?= $this->Html->script([
+        'jquery-slim.min',
+        'popper.min',
+        'bootstrap.min',
+    ]) ?>
+</body>
+</html>
