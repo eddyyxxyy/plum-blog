582ba26185958fa462fee65ebf2096458baff67c
Edson Pimenta <edson.tibo@gmail.com>
2023-01-18 08:53:31 -0300

    Feat: removes home.php and creates about_us page template

---
+++ plum_blog/templates/Pages/about_us.php
@@ -0,0 +1,29 @@
+<?php $this->assign('title', 'About Us') ?>
+
+<h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores officia voluptatum inventore ipsam saepe. <br>
+    <small>
+        <?php $this->assign('description', 'Asperiores natus harum veritatis nulla.') ?>
+        Asperiores natus harum veritatis nulla.
+    </small>
+</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores officia voluptatum inventore ipsam saepe. Asperiores natus harum veritatis nulla, tenetur dolor in vitae perspiciatis minus optio voluptas, alias, quod voluptates.
+</p>
+
+<p>
+    Ea dignissimos rerum laborum suscipit nostrum eum, expedita in corrupti repellendus modi facilis eaque vitae rem ullam dolorem. Est debitis asperiores at cupiditate magni iure dolore temporibus dignissimos blanditiis non!
+</p>
+
+<p>
+    Nobis recusandae, deleniti quas mollitia repellendus possimus odio quis explicabo placeat minima velit perspiciatis officiis laudantium dolore quos nisi nesciunt culpa ex dolorem. Dolorem ipsum quibusdam deleniti reprehenderit doloribus alias.
+</p>
+
+<p>
+    Qui sunt exercitationem ullam natus mollitia dolorem fuga tenetur dolor nostrum similique ipsam maiores, nemo enim nihil facilis fugiat ad voluptatibus odit unde illo architecto repellat nulla omnis! Expedita, beatae!
+</p>
+
+<p>
+    Explicabo libero mollitia deserunt nulla tempore sapiente, dolore eum iusto, optio atque minima rem! Deserunt consequuntur harum ut voluptates maxime praesentium, unde quod repellat culpa. Temporibus placeat voluptas dolor mollitia?
+</p>
