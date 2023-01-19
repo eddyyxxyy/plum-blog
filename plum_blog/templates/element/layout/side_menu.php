a2e0e703d86deb40681c14d6788909f7adc344f7
Edson Pimenta <edson.tibo@gmail.com>
2023-01-19 12:30:34 -0300

    Feat: adds public Newsletter form

--- plum_blog/templates/element/layout/side_menu.php
+++ plum_blog/templates/element/layout/side_menu.php
@@ -1,1 +1,9 @@
 <?= $this->cell('Categories') ?>
+
+<h2 class="mt-4">Newsletter</h2>
+<?php
+echo $this->Form->create(null, ['url' => ['controller' => 'Newsletters', 'action' => 'add']]);
+echo $this->Form->control('email', ['label' => 'E-mail']);
+echo $this->Form->button('Submit');
+echo $this->Form->end();
+?>
