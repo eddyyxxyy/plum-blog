975056709ad113e9cc00c35e78f438767643f701
Edson Pimenta <edson.tibo@gmail.com>
2023-01-19 22:07:15 -0300

    Feat: makes Profile and Change Password functional

--- plum_blog/templates/Admin/element/layout/upper_menu.php
+++ plum_blog/templates/Admin/element/layout/upper_menu.php
@@ -10,11 +10,23 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Edson Pimenta
+                        <?= $this->request->getSession()->read('Auth.User.first_name'); ?>
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Change password</a>
+                        <a class="dropdown-item" href="#">
+                            <?= $this->Html->link(
+                                'Profile',
+                                ['controller' => 'Users', 'action' => 'profile'],
+                                ['class' => 'dropdown-item']
+                            ); ?>
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <?= $this->Html->link(
+                                'Change Password',
+                                ['controller' => 'Users', 'action' => 'password'],
+                                ['class' => 'dropdown-item']
+                            ); ?>
+                        </a>
                         <div class="dropdown-divider"></div>
                         <?= $this->Html->link(
                             'Logout',
